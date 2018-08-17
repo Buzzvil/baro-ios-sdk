bc9684a40f483d0b36a998bfe3f5171299ea4716
koominjun <mjkoo711@gmail.com>
2018-08-17 14:24:40 +0900

    update sample app

---
+++ BuzzNativeSample/BuzzNativeSample/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  BuzzNativeSample
+//
+//  Created by MinJun KOO on 01/08/2018.
+//  Copyright © 2018 Buzzvil. All rights reserved.
+//
+
+import UIKit
+import BuzzNative
+
+class CustomTableViewCell: UITableViewCell  {
+  @IBOutlet weak var adView: BNAdView!
+}
